5184ec055e94630235946d445eacfb7a46c1e8b3
Ika Minami <ika@infiniteidol.com>
2026-09-10 07:29:51 +0100

    Hide raw rebuildSearch from the public core surface
    Adapters rebuild through rebuildDerived. Tests import rebuildSearch from
    the indexer module. This is one slice of #60.

--- packages/core/test/canon/write-capability.test.ts
+++ packages/core/test/canon/write-capability.test.ts
@@ -202,6 +202,7 @@ describe("canon write capability", () => {
     expect(Object.keys(core)).not.toContain("neighbors");
     expect(Object.keys(core)).not.toContain("purgeEvents");
     expect(Object.keys(core)).not.toContain("findPageById");
+    expect(Object.keys(core)).not.toContain("rebuildSearch");
     expect(Object.keys(core)).not.toContain("grantCanonWrite");
     expect(Object.keys(core)).not.toContain("openCanonFiles");
     expect(Object.keys(core)).not.toContain("assertCanonFiles");
